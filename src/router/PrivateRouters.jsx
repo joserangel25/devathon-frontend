e1fa43b00fea1fed0c5bfe883610530a4b565dc4
Denis Muñoz <denisenriquebaca@gmail.com>
2023-05-06 18:43:31 -0600

    :rocket: add comments to the private routes

--- src/router/PrivateRouters.jsx
+++ src/router/PrivateRouters.jsx
@@ -3,6 +3,6 @@ import { Navigate } from 'react-router-dom';
 
 export const PrivateRoutes = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
-
+  // for see the app without login uses !user ? children <Navigate to='/'/>
   return user ? children : <Navigate to='/' />;
 };
