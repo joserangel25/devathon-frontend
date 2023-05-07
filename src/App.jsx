23a74b5c7f7de884b620334d2f9463e821aa09c4
Kevin04C <kevincespedes2004@gmail.com>
2023-05-07 09:31:03 -0500

    Add react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
+      <Toaster />
     </Provider>
   );
 }
