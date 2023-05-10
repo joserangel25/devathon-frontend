bf51ee6d9acf8459e359e344f0620e63b0d4453f
joserangel25 <jdrangel00@gmail.com>
2023-05-09 21:40:35 -0500

    se incluye el FeedBack en la nueva ruta home

--- src/app/components/Modal-Side/index.jsx
+++ src/app/components/Modal-Side/index.jsx
@@ -2,10 +2,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 export const ModalSide = ({ title, toggleActive, children }) => {
   return (
-    <article
-      data-testid='feedback'
-      className='fixed top-0 right-0 bg-white min-h-screen max-w-sm p-9 z-[2] pt-16'
-    >
+    <article data-testid='feedback' className='fixed top-0 right-0 bg-white  md:max-w-sm p-9 z-[2]'>
       <div className='flex items-center justify-between  pb-6'>
         <h1 className='text-primary-900 font-bold text-[28px]'>{title}</h1>
         <AiOutlineCloseCircle
