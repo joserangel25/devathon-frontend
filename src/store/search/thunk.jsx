fb24fe4e521e3ee3520c596d747a5b196e5435ab
Denis Muñoz <denisenriquebaca@gmail.com>
2023-05-15 20:13:15 -0600

    :hammer: update thunk search axios

--- src/store/search/thunk.jsx
+++ src/store/search/thunk.jsx
@@ -1,16 +1,15 @@
 import { setResults, setIsLoading } from './searchSlice';
-import axios from 'axios';
+import LugarAccesibleApi from '../../api/LugarAccesibleApi';
 
 export const getResults = (searchObject) => {
   return async (dispatch) => {
     dispatch(setIsLoading()); // is loading to true
     dispatch(setResults([])); // set to default for every new request
     try {
-      // this is temporaly xd
-      const { data } = await axios.get(
-        `http://localhost:3000/search?lat=${searchObject.lat}&lng=${searchObject.lng}&query=${
-          searchObject.query
-        }&types=${searchObject.types === '' ? '""' : searchObject.types}`,
+      const { data } = await LugarAccesibleApi.get(
+        `search?lat=${searchObject.lat}&lng=${searchObject.lng}&query=${searchObject.query}&types=${
+          searchObject.types === '' ? '""' : searchObject.types
+        }`,
       );
       if (data) dispatch(setResults(data.data));
     } catch (error) {
